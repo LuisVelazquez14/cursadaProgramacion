2525c9b228d59dafea3e7b68724ec9b4ca1fa0bd
lucho <lucho@debian.local>
2020-08-31 01:35:10 -0300

    primera compilacion de cursada

---
+++ Clase1/src/Clase1a.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : Clase1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int numero;
+	int acumulador;
+
+	printf("ingrese un numero");/* prints !!!Hello World!!! */
+
+	scanf("%d", &numero);
+
+	printf("ingresaste %d",numero);
+
+
+
+	return EXIT_SUCCESS;
+}
